8448521a5f05f69547102b034327374576c3e23b
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 13:25:34 -0500

    Adds ocp-vpc dependency and updates tests
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-swaggereditor.tf
+++ test/stages/stage2-swaggereditor.tf
@@ -1,10 +1,10 @@
 module "dev_tools_swagger" {
   source = "./module"
 
-  cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
-  cluster_type             = module.dev_cluster.type_code
-  tls_secret_name          = module.dev_cluster.tls_secret_name
+  cluster_ingress_hostname = module.dev_cluster.platform.ingress
+  cluster_type             = module.dev_cluster.platform.type_code
+  tls_secret_name          = module.dev_cluster.platform.tls_secret
   releases_namespace       = module.dev_capture_state.namespace
   image_tag                = "v3.8.0"
   enable_sso               = true
